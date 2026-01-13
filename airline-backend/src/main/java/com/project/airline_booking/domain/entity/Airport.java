5b79ce06029fff93c904e2c106a31ba558d7c0c4
mkn <192448446+mika0798@users.noreply.github.com>
2026-01-13 17:39:03 +0700

    Complete Airport Entity

--- airline-backend/src/main/java/com/project/airline_booking/domain/entity/Airport.java
+++ airline-backend/src/main/java/com/project/airline_booking/domain/entity/Airport.java
@@ -1,5 +1,39 @@
 package com.project.airline_booking.domain.entity;
 
+import com.project.airline_booking.enums.City;
+import com.project.airline_booking.enums.Country;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@Entity
+@Table(name="airports")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private City city;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Country country;
+
+    @Column(unique = true, nullable = false,length = 3)
+    private String iataCode;
 
 }
