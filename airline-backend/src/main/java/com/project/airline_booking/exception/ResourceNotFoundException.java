ad6e0b71cdc28d1eb2a4c1e2ee637a03a4621277
mkn <192448446+mika0798@users.noreply.github.com>
2026-01-13 22:05:34 +0700

    Add resource not found exception

---
+++ airline-backend/src/main/java/com/project/airline_booking/exception/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.project.airline_booking.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
